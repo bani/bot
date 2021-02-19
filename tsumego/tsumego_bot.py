aa9c0b242144bc488b76b5427ec4bcbb13532e04
Vanessa Sabino <borboleta@gmail.com>
2021-02-18 22:44:18 -0500

    Tsumego Twitch

--- tsumego/tsumego_bot.py
+++ tsumego/tsumego_bot.py
@@ -1,9 +1,15 @@
 # bot.py
 import os # for importing env vars for the bot to use
+import re
 from twitchio.ext import commands
 
 import os
 from dotenv import load_dotenv
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
 load_dotenv()
 
 bot = commands.Bot(
@@ -32,7 +38,14 @@ async def event_message(ctx):
 
     await bot.handle_commands(ctx)
 
-    print(f"{ctx.author.name}: {ctx.content}")
+    # print(f"{ctx.author.name}: {ctx.content}")
+
+@bot.command(name='play')
+async def ping(ctx):
+    msg = ctx.message.clean_content
+    m = re.search(r'play ([a-z]{2})', msg)
+    place_stone(m.group(1))
+    await ctx.send('tap')
 
 @bot.command(name='ping')
 async def ping(ctx):
@@ -42,5 +55,24 @@ async def ping(ctx):
 async def ogs(ctx):
     await ctx.send('https://online-go.com/player/920382/')
 
+def fancy_click(id):
+    driver.execute_script("arguments[0].click();", driver.find_element_by_id(id))
+
+def solution_check():
+    solution_check = driver.find_element_by_id('solutionContainer')
+    if solution_check.text == 'Completed!':
+        fancy_click('loadButton')
+    elif solution_check.text == 'Wrong. Keep trying.':
+        driver.execute_script("stepBeginning()")
+
+def place_stone(coords):
+    fancy_click(coords)
+    time.sleep(2)
+    solution_check()
+
+
 if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.get('https://blacktoplay.com/?p=608')
     bot.run()
+
